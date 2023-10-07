ddb7379f7dd022f9de89da1310e36e6d5de5544c
Rafael Bertasso Borges <125775011+RafaelBertasso@users.noreply.github.com>
2023-10-07 15:13:14 -0300

    att ex 1

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,12 +3,15 @@ import random
 print("Bem vindo ao jogo de adivinhacao!")
 
 n_aleatorio = random.randint(1,5)
-n_usuario = int(input("Digite um numero de 1 a 5: "))
-print(f"Seu chute foi {n_usuario}")
-if n_aleatorio == n_usuario:
-    print("-----Você acertou!-----")
-elif n_usuario > 5:
-    print("Numero nao disponivel")
-else:
-    print("-----Voce errou!-----")
-    print(f"O numero era {n_aleatorio}")
+
+tentativas = 3
+for i in range(1,tentativas + 1):
+    n_usuario = int(input("Digite um numero de 1 a 5: "))
+    if n_aleatorio == n_usuario:
+        print("-----Você acertou!-----")
+        break
+    elif n_usuario > 5:
+        print("Numero nao disponivel")
+    else:
+        print("-----Voce errou!-----")
+print(f"O numero era {n_aleatorio}")
